f26407119b6d4ee40fc1b1c24a5d87ae5b4e87cd
Nick Mittu <nick@mittudev.com>
2017-04-08 08:21:04 -0400

    debug

--- hello.py
+++ hello.py
@@ -114,7 +114,7 @@ def home(strToConv):
 	output = _input.set_channels(1).set_frame_rate(16000)
 	f = output.export(tf.name, format="wav")"""
 	with open(file_name, 'rb') as audio_file:
-		return json.dumps(speech_to_text.recognize(audio_file, content_type="audio/L16; rate=16000; channels=1", timestamps=True, word_confidence=True))
+		return json.dumps(speech_to_text.recognize(audio_file, content_type="audio/basic", timestamps=True, word_confidence=True))
 
 
 
