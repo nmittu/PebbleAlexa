2e5bf34fe8cf3d9c5ae545f348909a301c1f0779
Nick Mittu <nick@mittudev.com>
2017-04-08 10:33:16 -0400

    pebble

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ import uuid
 from pydub import AudioSegment
 import tempfile
 import re
-import urllib
+import urllib.parse
 
 
 #Alexa
@@ -149,7 +149,7 @@ def code():
 	print(red.set(uid+"-access_token", resp['access_token']))
 	#red.expire(uid+"-access_token", 3600)
 	red.set(uid+"-refresh_token", resp['refresh_token'])
-	return "<head><script>function load(){document.location = \"pebblejs://close#" + urllib.quote(uid, safe='~()*!.\'') + "\";}</script></head><body onload=\"load();\"></body>"
+	return "<head><script>function load(){document.location = \"pebblejs://close#" + urllib.parse.quote(uid, safe='~()*!.\'') + "\";}</script></head><body onload=\"load();\"></body>"
 
 # /* Endpoint to greet and add a new visitor to database.
 # * Send a POST request to localhost:8080/api/visitors with body
