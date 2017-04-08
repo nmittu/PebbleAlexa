597c16047f34c384b1f31d0815c59708f06f5694
Nick Mittu <nick@mittudev.com>
2017-04-08 06:20:13 -0400

    debug

--- hello.py
+++ hello.py
@@ -58,8 +58,8 @@ def home(strToConv):
 	red = redis.from_url(redis_url)
 
 	uid = request.args.get("uid")
-	token = str(red.get(uid+"-access_token"))
 	return uid+"-access_token"
+	token = str(red.get(uid+"-access_token"))
 
 	url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
 	headers = {'Authorization' : 'Bearer %s' % token}
